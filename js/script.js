13f0876c8f854ffd5ffc7b6df79f76b3cddb4e66
HarrisonLoh <101675206+HSP22SCM79L@users.noreply.github.com>
2022-03-31 09:25:32 -0500

    basic event listener script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+  function clickAlertMe(){
+    alert("Harrison Loh: ")
+  }
+  var entrybtn = document.getElementById("entrybutton")
+  var text = document.getElementById("entryinput")
+  entrybtn.addEventListener('click',clickAlertMe)
 
 
 
-
-
-
-
+}
 window.addEventListener('load', init);
