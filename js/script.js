ed7e9df302fd770504e233a9ff77b9090fedfc02
HarrisonLoh <101675206+HSP22SCM79L@users.noreply.github.com>
2022-03-31 09:46:59 -0500

    Finish script to send alert and change h2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
+
   function clickAlertMe(){
-    alert("Harrison Loh: ")
+    var text = document.getElementById("entryinput").value;
+    alert("Harrison Loh: " + text);
+    document.getElementById("textoutput").innerHTML = text
   }
-  var entrybtn = document.getElementById("entrybutton")
-  var text = document.getElementById("entryinput")
-  entrybtn.addEventListener('click',clickAlertMe)
-
-
-
+  var entrybtn = document.getElementById("entrybutton");
+  entrybtn.addEventListener('click',clickAlertMe);
+  
 }
 window.addEventListener('load', init);
